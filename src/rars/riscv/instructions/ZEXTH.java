63d3d02d17b1eacbd1b227722909a087a770db90
Dmitrii Markov <markovdima43@gmail.com>
2022-04-25 17:56:51 +0300

    fix zext.h encoding

--- src/rars/riscv/instructions/ZEXTH.java
+++ src/rars/riscv/instructions/ZEXTH.java
@@ -1,16 +1,24 @@
 package rars.riscv.instructions;
 
-public class ZEXTH extends ImmediateEncodedInstruction{
+import rars.ProgramStatement;
+import rars.riscv.BasicInstruction;
+import rars.riscv.BasicInstructionFormat;
+
+public class ZEXTH extends BasicInstruction {
 
     public ZEXTH() {
         super("zext.h t1,t2", "Zero-extend the least-significant halfword in t2 and save it in t1",
-                "000010000000", "100");
+                BasicInstructionFormat.I_FORMAT, "000010000000 sssss 100 fffff 0110011");
         // TODO: change opcode in Verilog-code (it's R-type in specs for some reason and here it's I-type)
     }
 
-    @Override
-    protected long compute(long value, long immediate) {
-        // TODO: compute the result
-        return value;
+//    @Override
+//    protected long compute(long value, long immediate) {
+//        // TODO: compute the result
+//        return value;
+//    }
+
+    public void simulate(ProgramStatement statement) {
+
     }
 }
