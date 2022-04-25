1728a525ac91feb0a8b2d29eeec482354ac22329
Dmitrii Markov <markovdima43@gmail.com>
2022-04-25 17:34:26 +0300

    fix rori encoding and orc.b mnemonics

--- src/rars/riscv/instructions/RORI.java
+++ src/rars/riscv/instructions/RORI.java
@@ -1,16 +1,25 @@
 package rars.riscv.instructions;
 
-public class RORI extends ImmediateEncodedInstruction {
+import rars.ProgramStatement;
+import rars.riscv.BasicInstruction;
+import rars.riscv.BasicInstructionFormat;
+import rars.riscv.hardware.RegisterFile;
+
+public class RORI extends BasicInstruction {
 
     public RORI() {
-        super("rori t1,t2,t3", "Right rotation of t2 by the amount in least-significant 5 bits of shamt. The result saved in t1",
-                "0110000", "101");
+        super("rori t1,t2,10", "Right rotation of t2 by the amount in least-significant 5 bits of shamt. The result saved in t1",
+                BasicInstructionFormat.I_FORMAT, "0110000 ttttt sssss 101 fffff 0010011");
         // TODO: write new ShamtInstruction abstract class for this class to inherit
     }
 
-    @Override
-    public long compute(long value, long immediate) {
-        return Long.rotateRight(value, (int) immediate & 0X1F);
+//    @Override
+//    public long compute(long value, long immediate) {
+//        return Long.rotateRight(value, (int) immediate & 0X1F);
+//    }
+
+    public void simulate(ProgramStatement statement) {
+
     }
 }
 
